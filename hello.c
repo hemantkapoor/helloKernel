f79af0930f5f0895a582add03893d3c1e39d0419
hemantkapoor <kapoor.hemant@gmail.com>
2018-03-11 19:45:45 +0000

    Updated code to allocate device number dynamically

--- hello.c
+++ hello.c
@@ -1,18 +1,36 @@
 #include <linux/module.h>       /* Needed by all modules */
 #include <linux/kernel.h>       /* Needed for KERN_INFO */
 #include <linux/init.h>         /* Needed for the macros */
+#include <linux/fs.h>           /* Major and Minor number*/
+
+static dev_t first;
 
 static int __init hello_start(void)
 {
-printk(KERN_INFO "Loading hello module...\n");
-printk(KERN_INFO "Hello world\n");
-return 0;
+	printk(KERN_INFO "Loading hello module...\n");
+	printk(KERN_INFO "Hello world\n");
+	//We will now try to allocate dynamic major and minor number and print it
+	printk(KERN_INFO "Getting Device Number");
+	int ret;
+	if((ret = alloc_chrdev_region(&first,0,3,"scull")) < 0)
+	{
+		printk(KERN_INFO "Unable to get Device Number");
+		return ret;
+	}
+	//If we are here then we got the device number
+	printk(KERN_INFO "<MAJOR , MINOR ><%d %d>\n" , MAJOR(first),MINOR(first));
+	return 0;
 }
 
 static void __exit hello_end(void)
 {
-printk(KERN_INFO "Goodbye Mr.\n");
+	//We free the device number here
+	unregister_chrdev_region(first,3);
+	printk(KERN_INFO "Goodbye Mr.\n");
 }
 
 module_init(hello_start);
 module_exit(hello_end);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Hemant Kapoor");
+MODULE_DESCRIPTION("Simple Hello Kernel");
